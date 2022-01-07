b57f26a3a127caa43bca821904806cc35ef3c032
githubyunhan <240791094@qq.com>
2022-01-07 23:04:28 +0800

    22.store文件夹中的index.js中内容的抽离与封装

---
+++ src/store/actions.js
@@ -0,0 +1,15 @@
+export default {
+  addCart1(context,payload) {
+    /*1.查找之前数组中是否有该商品*/
+    let oldProduct = context.state.cartList.find(item => item.iid === payload.iid);
+
+    /*2.判断oldProduct*/
+    if (oldProduct) {
+      context.commit('addCounter',oldProduct)
+    } else  {
+      payload.count = 1
+      /*context.state.cartList.push(payload)*/
+      context.commit('addToCart',payload)
+    }
+  }
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,25 @@
+export default {
+  addCounter(state,payload) {
+    payload.count++
+  },
+  addToCart(state,payload) {
+    state.cartList.push(payload)
+  },
+
+  addCart1(state,payload) {
+    let oldProduct = null;
+
+    for (let item of state.cartList) {
+      if (item.iid === payload.iid) {
+        oldProduct = item;
+      }
+    }
+
+    if (oldProduct) {
+      oldProduct.count += 1
+    } else  {
+      payload.count = 1
+      state.cartList.push(payload)
+    }
+  }
+}
